5f6d24a65672ba8ef0f722ea8516001327eedc55
Renan Torres <rtorres@daitan.com>
2022-05-27 10:59:22 -0300

    [GKEEP]: Configured Webpack Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,42 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: path.resolve(__dirname, "src/index.js"),
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname,"dist")
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   output: {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    assetModuleFilename: '[name][ext]'
   },
-  plugins : [new HtmlWebpackPlugin({
-    template: "./template.html"
-  })],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Google Keep",
+      output: "index.html",
+      template: "./template.html",
+    }),
+  ],
 };
